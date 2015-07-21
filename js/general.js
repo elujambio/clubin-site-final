7d9fe8ff6d86d7ce2519eae2ab98169c0c29f49f
elujambio <eugeniolujambio@gmail.com>
2015-07-20 19:16:00 -0500

    Running at 60FPS
    Built site base to run in 60fps

--- js/general.js
+++ js/general.js
@@ -1,8 +1,10 @@
 var navbarMenu = document.getElementById("navbarMenu");
 var menuSpritesheet = document.getElementById("menuSpritesheet");
 var menu = document.getElementById("menu");
+var oneThreeImg = document.querySelector("#oneThree img");
 var toggle = 1;
-
+var body = document.getElementsByTagName("body");
+var glyphSmall = document.getElementById("glyphSmall");
 navbarMenu.addEventListener("click", toggleMenu);
 
 function toggleMenu(){
@@ -18,3 +20,32 @@ function toggleMenu(){
 		toggle=0;
 	}
 }
+
+body[0].onload=function(){
+	oneThreeImg.className = oneThreeImg.className + " show"; 
+};
+
+function scrollHandler(){
+	// if(window.pageYOffset >= document.clientHeight - 100){
+	// 	glyphSmall.style.fill = "#cdb378";
+	// }
+	var counter = 0;
+    if(window.pageYOffset >= (one.clientHeight - one.offsetTop - navbar.clientHeight)){
+    	counter++;
+    }
+    if(window.pageYOffset <= (one.clientHeight - one.offsetTop - navbar.clientHeight)){
+    	counter--;
+    }
+    if(counter>0  && counter<2){
+    	glyphSmall.style.fill = "#cdb378";
+    	glyphSmall.style.transition = "fill .4s ease";
+    	menuSpritesheet.src="img/clubin-menu-dark.svg";
+    }
+    if(counter<0 && counter>-2){
+    	glyphSmall.style.fill = "url(#gradient)";
+    	glyphSmall.style.transition = "fill .4s ease";
+    	menuSpritesheet.src="img/clubin-menu.svg";    
+    }    	
+}
+window.onscroll = scrollHandler;
+// body[0].addEventListener("scroll", scrollHandler);
